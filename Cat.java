635ab0a888dbaeae73d3141a430e0cfb04cdb6d4
Pavel Grachev <pagr89@gmail.com>
2017-06-26 16:32:06 +0200

    Cat lesson 2 homework. Cat class changes.

---
+++ Cat.java
@@ -0,0 +1,64 @@
+
+public class Cat
+{
+    private Double originWeight; // изначальный вес
+    private Double weight;
+    private Double foodAmount;
+
+    private Double minWeight;
+    private Double maxWeight;
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+
+    }
+
+    public Double getFoodAmount() { return foodAmount; }
+
+    public void toilet()
+    {
+        weight = weight - 20;
+        System.out.println("Don't forget to splash!");
+    }
+
+    public void meow()
+    {
+        weight = weight - 1;
+        System.out.println("Meow");
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
