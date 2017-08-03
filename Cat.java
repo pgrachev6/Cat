0dec952a923845114f235e659db8598770706023
Pavel Grachev <pagr89@gmail.com>
2017-08-03 15:23:46 +0200

    Cat lesson 4 homework. Loader and Cat class changes after seminar.

--- Cat.java
+++ Cat.java
@@ -1,15 +1,27 @@
 
 public class Cat
 {
-    private Double originWeight; // изначальный вес
-    private Double weight;
-    private Double foodAmount;
-    private Double minWeight;
-    private Double maxWeight;
-    private static Integer counter = 0;
+    private double originWeight; // изначальный вес
+    private double weight;
+    private double foodAmount;
+    private double minWeight;
+    private double maxWeight;
+    private static int counter;
+    private String name;
 
-    public Cat()
-    {
+    public String getName() {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public static int getCount() {
+        return counter;
+    }
+
+    public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
@@ -17,79 +29,55 @@ public class Cat
         counter++;
     }
 
-    public Cat(Double weight)
-    {
+    public Cat(double weight) {
+        setWeight(weight);
+        counter++;
+    }
+
+    // set weight method
+    public void setWeight(double weight) {
         this.weight = weight;
-        counter++;
     }
 
-    //Create a twin by the parameters
-    public Cat createTwin() {
-        this.weight = 1500.0 + 3000.0 * Math.random();
-        this.originWeight = weight;
-        this.minWeight = 1000.0;
-        this.maxWeight = 9000.0;
-        counter++;
-        Cat cat = new Cat();
-        return cat;
+    public double getWeight() {
+        return weight;
     }
 
-    public static Integer getCount()
-    {
-        return counter;
-    }
-
-    public static Double getWeightDifference(Cat cat1, Cat cat2)
-    {
-        Double difference = Math.abs(cat1.getWeight() - cat2.getWeight());
-        return difference;
-    }
-
-    //toilet method
-    public void toilet()
-    {
-        weight = weight - 20;
+      //toilet method
+    public void toilet() {
+        setWeight(weight - 20);
         System.out.println("Don't forget to splash!");
     }
 
-    public void meow()
-    {
-        weight = weight - 1;
+     public static double getWeightDifference(Cat cat1, Cat cat2) {
+        double difference = Math.abs(cat1.getWeight() - cat2.getWeight());
+        return difference;
+    }
+
+    public void meow() {
+        setWeight(weight - 1);
         System.out.println("Meow");
     }
 
-    public void meow(Double weight)
-    {
-        this.weight = this.weight - weight;
+    public void meow(double weight) {
+        setWeight(this.weight - weight);
         System.out.println("Meow");
     }
 
-    public void feed(Double amount)
-    {
+    public void feed(double amount) {
         weight = weight + amount;
         foodAmount = amount;
     }
 
-    public  Double getFoodAmount() { return foodAmount; }
+    public double getFoodAmount() {
+        return foodAmount;
+    }
 
-    public void drink(Double amount)
-    {
+    public void drink(double amount) {
         weight = weight + amount;
     }
 
-    public Double getWeight()
-    {
-        return weight;
-    }
-
-    // set weight method
-    public void setWeight(Double weight)
-    {
-        this.weight = weight;
-    }
-
-    public String getStatus()
-    {
+    public String getStatus() {
         if(weight < minWeight) {
             counter--;
             return "Dead";
